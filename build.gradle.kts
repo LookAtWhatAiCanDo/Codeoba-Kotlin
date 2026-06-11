7ec574d7d0dbdf02cf2ddd323f8638fb88e20cdb
Paul Peavyhouse <pv@swooby.com>
2026-06-10 20:23:03 -0700

    chore: Upgrade Kotlin, Compose, and Gradle versions
    Update Kotlin and Compose Multiplatform to their latest stable versions for
    bug fixes, performance improvements, and new features. This commit also
    upgrades the Gradle wrapper to 8.14.4, including the updated execution
    scripts (`gradlew` and `gradlew.bat`) for compatibility with newer Gradle releases.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("multiplatform") version "2.0.21" apply false
-    kotlin("plugin.serialization") version "2.0.21" apply false
-    id("org.jetbrains.compose") version "1.7.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    kotlin("multiplatform") version "2.2.10" apply false
+    kotlin("plugin.serialization") version "2.2.10" apply false
+    id("org.jetbrains.compose") version "1.11.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
 
 allprojects {
